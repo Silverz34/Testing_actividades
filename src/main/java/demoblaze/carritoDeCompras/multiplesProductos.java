a2b0625ab012e8a24ee5258695575cea0321c24c
Silverz34 <pradogordillomichael@gmail.com>
2026-03-02 10:14:09 -0600

    no se si sea factible

---
+++ src/main/java/demoblaze/carritoDeCompras/multiplesProductos.java
@@ -0,0 +1,12 @@
+package demoblaze.carritoDeCompras;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.List;
+
+public class multiplesProductos {
+
+}
